4fd686d01642d12270f28cfae87c9d56bdb10540
ktanaka <ktanaka@tanaka.ecc.u-tokyo.ac.jp>
2014-09-18 17:19:32 +0900

    add unit test files

---
+++ testPlayer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import unittest
+from Player import Player
+
+class TestPalyer(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+
